d1b9a722359686ff14b30668354cfdd37104dfc9
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-06-17 20:14:46 +0900

    create 02 ~ 05 dirs

---
+++ 04/main.py
@@ -0,0 +1,38 @@
+MENU   = 'menu'
+GAME   = 'game'
+
+scene = MENU
+
+mapping = ['village', 'mountain', 'hills', 'fields', 'mountain', 'mayor', 'hills', 'fields', 'cave']
+pos = 0
+
+while True:
+    if scene == MENU:
+        print("----------------------------------------")
+        print('1: Play game')
+        print('2: Quit game')
+        print("----------------------------------------")
+        choice = input('>> ')
+        if choice == '1':
+            print('What is your name?')
+            name = input('>> ')
+            scene = GAME
+        else:
+            exit()
+
+    elif scene == GAME:
+        tile = mapping[pos]
+        print("----------------------------------------")
+        print(f"Tile: {tile}")
+        print("----------------------------------------")
+        print('1: Walk')
+        print('2: Save and quit game')
+        print('3: Stay here')
+        print("----------------------------------------")
+        choice = input('>> ')
+        if choice == '1':
+            pass
+        elif choice == '2':
+            exit()
+        elif choice == '3':
+            pass
