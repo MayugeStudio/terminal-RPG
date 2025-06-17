465ce1aa7825469109c685fcfbfa5236a036dea0
MayugeStudio <my.favorite.is.energy@gmail.com>
2025-06-17 18:56:46 +0900

    Fix spelling

--- 01/main.py
+++ 01/main.py
@@ -5,16 +5,16 @@ scene = MENU
 
 if scene == MENU:
     print("----------------------------------------")
-    print('1: play game')
-    print('2: quit game')
+    print('1: Play game')
+    print('2: Quit game')
     print("----------------------------------------")
     input('#')
 
 elif scene == GAME:
     print("----------------------------------------")
-    print('1: walk')
-    print('2: run')
-    print('3: save game')
+    print('1: Walk')
+    print('2: Run')
+    print('3: Save and quit game')
     print("----------------------------------------")
     input('#')
 
